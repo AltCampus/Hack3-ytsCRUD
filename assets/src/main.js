cdb2cba5d1001e002f2765d8fe433f21d21b6c05
ashwanig3 <goswami17829@gmail.com>
2018-10-28 16:07:33 +0530

    get js data

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,8 +1,5 @@
 var allMoviesList = [];
+var movieContainer = document.querySelector(".movie-container ul")
 var url = "https://yts.am/api/v2/list_movies.json";
 
-fetch(url)
-        .then(res => res.json())
-        .then(data => allMoviesList.push(data));
-
- console.log(allMoviesList)
+	
