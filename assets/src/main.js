17dff951f0b230045bb60dff0594fd0c56b50ff2
Abhishek Shastri <shastri48@gmail.com>
2018-10-28 16:37:55 +0530

    add search function

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,17 +1,19 @@
 var allMoviesList = [];
 var  moviesContainer = document.querySelector(".movies__container ul");
+var searchMovies = document.querySelector("[name=site-search]")
 var url = "https://yts.am/api/v2/list_movies.json";
 
 function fetchData() {
   fetch(url)
     .then(res => res.json())
     .then(data => allMoviesList.push(...data.data.movies))
+    .then(data => showData(allMoviesList));
 
-}
+  }
 fetchData();
 
-function showData() {
-  moviesContainer.innerHTML = allMoviesList.map(v => {
+function showData(moviestext) {
+  moviesContainer.innerHTML = moviestext.map(v => {
     return `<li>
       <img src = "${v.medium_cover_image}">
       <h3>${v.title_english}</h3>
@@ -21,4 +23,11 @@ function showData() {
   }).join('');      
 }
 
+function search(e) {
+  let searchText = e.target.value.toLowerCase();
+  var newArray = allMoviesList.filter(v => v.title_english.toLowerCase().includes(searchText));
+  showData(newArray);
+}
 
+
+searchMovies.addEventListener("keyup", search);
