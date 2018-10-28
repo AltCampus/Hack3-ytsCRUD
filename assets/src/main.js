9c78782ca51f2f570d38b5d996566ccf65a93763
Abhishek Shastri <shastri48@gmail.com>
2018-10-28 16:14:21 +0530

    fetch html data

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,11 +1,13 @@
 var allMoviesList = [];
 var  moviesContainer = document.querySelector(".movies__container ul");
+var searchMovies = document.querySelector("")
 var url = "https://yts.am/api/v2/list_movies.json";
 
 function fetchData() {
   fetch(url)
     .then(res => res.json())
     .then(data => allMoviesList.push(...data.data.movies))
+    .then(showData());
 
 }
 fetchData();
@@ -21,4 +23,13 @@ function showData() {
   }).join('');      
 }
 
+function search(e) {
+  let searchText = e.target.value.toLowerCase();
+  var newArray = books.filter(v => v.toLowerCase().includes(searchText));
+  displayBook(newArray);
+}
+
+
+
+searchMovies.addEventListener("keyup", search);
 
